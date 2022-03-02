f91b6ba81f78833044db4206ce5468c567bf222d
Pooya Ashtari <pooya.ash@gmail.com>
2022-03-02 20:09:16 +0100

    polish

--- factorizer/utils/schedulers.py
+++ factorizer/utils/schedulers.py
@@ -5,24 +5,24 @@ from torch.optim.lr_scheduler import LambdaLR
 
 class WarmupCosineSchedule(LambdaLR):
     """ Linear warmup and then cosine decay.
-        Linearly increases learning rate from 0 to 1 over `warmup_epochs` training epochs.
-        Decreases learning rate from 1. to 0. over remaining epochs.
+        Linearly increases learning rate from 0 to 1 over `warmup_steps` training steps.
+        Decreases learning rate from 1. to 0. over remaining steps.
     """
 
-    def __init__(self, optimizer, warmup_epochs, total_epochs, last_epoch=-1):
+    def __init__(self, optimizer, warmup_steps, total_steps, last_step=-1):
 
-        self.warmup_epochs = warmup_epochs
-        self.total_epochs = total_epochs
+        self.warmup_steps = warmup_steps
+        self.total_steps = total_steps
         super(WarmupCosineSchedule, self).__init__(
-            optimizer, self.lr_lambda, last_epoch=last_epoch
+            optimizer, self.lr_lambda, last_step=last_step
         )
 
-    def lr_lambda(self, epoch):
-        if epoch < self.warmup_epochs:
-            factor = epoch / self.warmup_epochs
+    def lr_lambda(self, step):
+        if step < self.warmup_steps:
+            factor = step / self.warmup_steps
         else:
-            progress = epoch - self.warmup_epochs  # progress after warmup
-            w = math.pi / (self.total_epochs - self.warmup_epochs)  # frequency
+            progress = step - self.warmup_steps  # progress after warmup
+            w = math.pi / (self.total_steps - self.warmup_steps)  # frequency
             factor = 0.5 * (1.0 + math.cos(w * progress))
 
         return factor
